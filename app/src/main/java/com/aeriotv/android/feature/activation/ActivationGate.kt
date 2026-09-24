00a3c9b1cfe769725898d792734e0b4fcbd7f89c
kyanik010 <kyaniky01@gmail.com>
2026-09-25 00:23:21 +0400

    fix: clean activation screen imports

--- app/src/main/java/com/aeriotv/android/feature/activation/ActivationGate.kt
+++ app/src/main/java/com/aeriotv/android/feature/activation/ActivationGate.kt
@@ -11,13 +11,13 @@ import android.graphics.BitmapFactory
 import androidx.compose.foundation.background
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
