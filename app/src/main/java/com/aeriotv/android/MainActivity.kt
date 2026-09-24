99d8c45aac93bb97ae896ab735d9c7f3685f4272
kyanik010 <kyaniky01@gmail.com>
2026-09-25 02:34:33 +0400

    fix: apply language provider without changing activity structure

--- app/src/main/java/com/aeriotv/android/MainActivity.kt
+++ app/src/main/java/com/aeriotv/android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aeriotv.android
 import android.app.PictureInPictureParams
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import android.os.SystemClock
